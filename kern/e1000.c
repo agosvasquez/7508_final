256c6836378fb22238b6c33b61454130b132ce13
franhermani <franhermani@gmail.com>
2019-02-06 16:56:36 -0300

    Ejercicio 4 resuelto

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,12 +1,14 @@
 #include <kern/e1000.h>
 #include <kern/pci.h>
+#include <kern/pmap.h>
 
 // LAB 6: Your driver code here
 
 // VA del BAR 0
-volatile uint32_t *e1000_bar0;
+volatile uint8_t *e1000_bar0;
 
-int e1000_attach(struct pci_func *pcif) {
+int
+e1000_attach(struct pci_func *pcif) {
 	// Habilito el E1000 device
 	pci_func_enable(pcif);
 
@@ -15,7 +17,16 @@ int e1000_attach(struct pci_func *pcif) {
 	uint32_t reg_size0 = pcif->reg_size[0];
 	e1000_bar0 = mmio_map_region(reg_base0, reg_size0);
 
-	uint32_t status = 
-	printf("%u", )
+	// Compruebo que el status es el correcto (0x80080783)
+	//uint32_t status = e1000_getreg(E1000_STATUS);
+	//cprintf("0x%x\n", status);
+
 	return 0;
+}
+
+
+static uint32_t
+e1000_getreg(uint32_t offset)
+{
+  return *(volatile uint32_t *) (e1000_bar0 + offset);
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -3,6 +3,10 @@
 
 #include <kern/pci.h>
 
+#define E1000_STATUS   0x00008  /* Device Status - RO */
+
 int e1000_attach(struct pci_func *pcif);
+
+static uint32_t e1000_getreg(uint32_t offset);
 
 #endif  // JOS_KERN_E1000_H
