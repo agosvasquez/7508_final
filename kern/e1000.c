7b0ac30e479bb12aa715b5656e4967d353feaec8
franhermani <franhermani@gmail.com>
2019-02-08 20:47:05 -0300

    Empieza el ej 5

--- kern/e1000.c
+++ kern/e1000.c
@@ -7,6 +7,11 @@
 // VA del BAR 0
 volatile uint8_t *e1000_bar0;
 
+// Array de Transmit Descriptors
+struct e1000_tx_desc *tx_descriptors = NULL;
+
+
+// Inicializa el E1000
 int
 e1000_attach(struct pci_func *pcif) {
 	// Habilito el E1000 device
@@ -18,15 +23,47 @@ e1000_attach(struct pci_func *pcif) {
 	e1000_bar0 = mmio_map_region(reg_base0, reg_size0);
 
 	// Compruebo que el status es el correcto (0x80080783)
-	//uint32_t status = e1000_getreg(E1000_STATUS);
-	//cprintf("0x%x\n", status);
+	uint32_t status = e1000_getreg(E1000_STATUS);
+	cprintf("0x%x\n", status);
+
+	// Inicializo la transmit queue
+	e1000_init_transmit_queue();
+
+
+	// Inicializo la receive queue
+	//e1000_init_receive_queue();
 
 	return 0;
 }
 
+void
+e1000_init_transmit_queue(void) {
+	// 1) 	Alocar una porción de memoria para el array de transmit descriptors y
+	// 		para los buffers packets apuntados por los descriptors.
+	// 		Dos alternativas:
+	//			a) Alocar paginas dinamicamente
+	//			b) Declarar variables globales
+	//		Importante:
+	//			- Los buffers deben ser contiguos en memoria fisica
+	//			- Alinear la memoria a 16 bytes
+	//			- El array consta de un multiplo de 8 descriptors
+	//			- El array tiene como maximo 64 descriptors
+	//			- Cargar address y length de los transmit descriptors
 
+	for (i = NTXDESC; i > 0; i--) {
+		// Armo la lista enlazada de envs libres de modo tal que
+		// en la primera llamada a env_init --> env_free_list = envs[0]
+		//envs[i - 1].env_status = ENV_FREE;
+		//envs[i - 1].env_id = 0;
+		//envs[i - 1].env_link = env_free_list;
+		//env_free_list = &envs[i - 1];
+	}
+}
+
+
+// Obtiene el valor del registro e1000_bar0[offset]
+// con aritmetica de punteros
 static uint32_t
-e1000_getreg(uint32_t offset)
-{
+e1000_getreg(uint32_t offset) {
   return *(volatile uint32_t *) (e1000_bar0 + offset);
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -3,10 +3,58 @@
 
 #include <kern/pci.h>
 
-#define E1000_STATUS   0x00008  /* Device Status - RO */
 
+/*------------*/
+/* Constantes */
+/*------------*/
+
+#define E1000_STATUS   		0x00008		/* Device Status - RO */
+#define E1000_TXD_CMD_DEXT	0x20000000	/* Descriptor extension (0 = legacy) */
+#define NTXDESC				56			/* Cantidad maxima de transmit descriptors en el array */
+
+/*-------------*/
+/* Estructuras */
+/*-------------*/
+
+// Transmit Descriptor - Legacy Mode
+// Seccion 3.3.3 - Pag. 36
+// e1000_hw.h
+struct e1000_tx_desc {
+    uint64_t buffer_addr;       /* Address of the descriptor's data buffer */
+    union {
+        uint32_t data;
+        struct {
+            uint16_t length;    /* Data buffer length */
+            uint8_t cso;        /* Checksum offset */
+            uint8_t cmd;        /* Descriptor control */
+        } flags;
+    } lower;
+    union {
+        uint32_t data;
+        struct {
+            uint8_t status;     /* Descriptor status */
+            uint8_t css;        /* Checksum start */
+            uint16_t special;
+        } fields;
+    } upper;
+};
+
+// Receive Descriptor
+// ...
+// e1000_hw.h
+struct e1000_rx_desc {
+    uint64_t buffer_addr; /* Address of the descriptor's data buffer */
+    uint16_t length;     /* Length of data DMAed into data buffer */
+    uint16_t csum;       /* Packet checksum */
+    uint8_t status;      /* Descriptor status */
+    uint8_t errors;      /* Descriptor Errors */
+    uint16_t special;
+};
+
+
+/*-----------*/
+/* Funciones */
+/*-----------*/
 int e1000_attach(struct pci_func *pcif);
-
-static uint32_t e1000_getreg(uint32_t offset);
 
 #endif  // JOS_KERN_E1000_H
