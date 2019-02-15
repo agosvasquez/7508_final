9087fe02a537ee9030019806e22cfad0e3f1cc5b
franhermani <franhermani@gmail.com>
2019-02-15 14:56:23 -0300

    Prototipo de la funcion output()

--- net/output.c
+++ net/output.c
@@ -10,4 +10,25 @@ output(envid_t ns_envid)
 	// LAB 6: Your code here:
 	// 	- read a packet from the network server
 	//	- send the packet to the device driver
+	
+	int r;
+	
+	// Loop
+	while (...) {
+		// Los NSREQ_OUTPUT IPC's se envian con la funcion low_level_output()
+		// Definida en net/lwip/jos/jif/jif.c
+		// ...
+		
+		// Cada IPC incluye una pagina tipo union Nsipc con el packet en su struct jif_pkt 'pkt'
+		// ...
+		
+		// Aceptar mensajes NSREQ_OUTPUT IPC del core network server
+		// Enviar packets acompañando dichos mensajes al network device driver
+		r = sys_send_packet(...);
+		
+		// Contemplar el caso que la transmit queue este llena
+		if (r < 0) {
+			// ...
+		}
+	}
 }
