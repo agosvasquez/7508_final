36953cd3957e7b8b542d4e2a06145e7828402870
franhermani <franhermani@gmail.com>
2019-02-17 18:17:51 -0300

    Se agrega el spin time en input.c

--- net/input.c
+++ net/input.c
@@ -1,5 +1,7 @@
 #include "ns.h"
 
+#define SPIN_TIME	10
+
 extern union Nsipc nsipcbuf;
 
 void
@@ -13,4 +15,24 @@ input(envid_t ns_envid)
 	// Hint: When you IPC a page to the network server, it will be
 	// reading from it for a while, so don't immediately receive
 	// another packet in to the same physical page.
+
+	int i, r;
+	
+	// Loop infinito
+	while (1) {
+		// Leo un paquete del driver
+		nsipcbuf.pkt.jp_data[0] = 'a';
+		if ((r = sys_network_recv(nsipcbuf.pkt.jp_data, nsipcbuf.pkt.jp_len)) < 0) {
+			// TODO: ver que hago aca
+		}
+
+		// Envio el paquete al network server
+		ipc_send(ns_envid, NSREQ_INPUT, &nsipcbuf.pkt, PTE_P | PTE_W | PTE_U);
+
+		// Spin para esperar que el network server lea correctamente el paquete actual
+		// antes de leer el siguiente en la misma pagina fisica
+		for (i = 0; i < SPIN_TIME; i++) {
+			sys_yield();
+		}
+	}
 }
